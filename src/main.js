944227c794bb6639692796354a6c5a1b581a55a1
Chris Cuenco <chrisviemric.cuenco@gmail.com>
2026-09-14 20:12:14 +0800

    deleted game.js to reduce hard to find bug errors and for a more cleaner js

--- src/main.js
+++ src/main.js
@@ -9,6 +9,100 @@ const resetBtn = document.getElementById("reset");
 
 const entryError = document.getElementById("errorA");
 
+const tiles = document.querySelectorAll(".tile");
+const current = document.getElementById("currentPlayer");
+
+const tileArray = [];
+const winningCombinations = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
+
+function winner(tileArray){
+    let winner;
+
+    winningCombinations.forEach(index => {
+        const first = tileArray[index[0]].textContent;
+        const second = tileArray[index[1]].textContent;
+        const third = tileArray[index[2]].textContent;
+        
+        if(first !== "" && second !== "" && third !== ""){
+            if(first === second && second === third){
+                if(first === "X"){
+                    winner = "X";
+                    return;
+                } else {
+                    winner = "O";
+                    return;
+                }
+            }
+        }
+    });
+
+    return winner;
+}
+
+function play(){
+  let firstSymbol = this.player[0][1];
+  let secondSymbol  = this.player[1][1];
+  let currentPlayer = firstSymbol;
+  current.textContent = "Turn for " + currentPlayer;
+  (currentPlayer === firstSymbol) ?  current.style.color = "#BFCC94" : current.style.color = "#C1292E";
+
+  tiles.forEach(tile => {
+      tileArray.push(tile);
+
+      tile.addEventListener("click", () => {
+          if(tile.textContent !== ""){
+              return;
+          }
+
+          if(currentPlayer === "X"){
+              tile.style.color = "#BFCC94";
+          } if(currentPlayer === "O"){
+              tile.style.color = "#C1292E";
+          }
+          
+          tile.textContent = currentPlayer;
+
+          const win = winner(tileArray);
+
+          if(win === "X"){
+              console.log("X won");
+              return;
+          } else if(win === "O"){
+              console.log("O won");
+              return;
+          } else {
+              const draw = tileArray.every(tile => {
+                  return tile.textContent !== ""
+              });
+
+              if(draw){
+                  console.log("Draw");
+                  return;
+              }
+          }
+
+          currentPlayer = (currentPlayer === firstSymbol) ? secondSymbol : firstSymbol;
+          current.textContent = "Turn for " + currentPlayer;
+          (currentPlayer === firstSymbol) ?  current.style.color = "#BFCC94" : current.style.color = "#C1292E";
+      });
+  });
+}
+
+function stop(){
+  tiles.forEach(tile => {
+      tile.textContent = "";
+  });
+}
+
 function changeAnimation(first, second){
   first.style.opacity = '0';
   setTimeout(() => {
@@ -18,11 +112,19 @@ function changeAnimation(first, second){
   }, 1000);
 }
 
-function getPlayer(){
-  const firstPlayer = document.getElementById("firstPlayerName").value;
-  const secondPlayer = document.getElementById("secondPlayerName").value;
-  const firstPlayerSymbol = document.getElementById("firstPlayerSymbol").value;
-  const secondPlayerSymbol = document.getElementById("secondPlayerSymbol").value;
+function getPlayer(action){
+  let firstPlayer = document.getElementById("firstPlayerName").value;
+  let secondPlayer = document.getElementById("secondPlayerName").value;
+  let firstPlayerSymbol = document.getElementById("firstPlayerSymbol").value;
+  let secondPlayerSymbol = document.getElementById("secondPlayerSymbol").value;
+
+  if(action === "stop"){
+    firstPlayer = "";
+    firstPlayerSymbol = "";
+    secondPlayer = "";
+    secondPlayerSymbol = "";
+    return;
+  }
   
   if(firstPlayer === "" || secondPlayer === "" || firstPlayerSymbol === secondPlayerSymbol){
     entryError.classList.remove("hidden");
@@ -54,7 +156,7 @@ playBtn.addEventListener("click", () => {
 });
 
 resetBtn.addEventListener("click", () => {
-  const game = getPlayer();
+  const game = getPlayer("stop");
 
   changeAnimation(gameBoard, entryPlayer);
 
