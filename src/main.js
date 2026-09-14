7fc097763b67641969f79ac75a1bcdbac8ba0e0c
Chris Cuenco <chrisviemric.cuenco@gmail.com>
2026-09-14 21:00:51 +0800

    fixed the reset button bug

--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
 import './style.css'
-import Game from './game.js';
 
 const entryPlayer = document.getElementById("enterPlayers");
 const gameBoard = document.getElementById("mainGame");
@@ -12,7 +11,6 @@ const entryError = document.getElementById("errorA");
 const tiles = document.querySelectorAll(".tile");
 const current = document.getElementById("currentPlayer");
 
-const tileArray = [];
 const winningCombinations = [
     [0, 1, 2],
     [3, 4, 5],
@@ -23,6 +21,15 @@ const winningCombinations = [
     [0, 4, 8],
     [2, 4, 6]
 ];
+
+function changeAnimation(first, second){
+  first.style.opacity = '0';
+  setTimeout(() => {
+    first.classList.add("hidden");
+    second.classList.remove("hidden");
+    second.style.opacity = '100';
+  }, 1000);
+}
 
 function winner(tileArray){
     let winner;
@@ -49,116 +56,102 @@ function winner(tileArray){
 }
 
 function play(){
-  let firstSymbol = this.player[0][1];
-  let secondSymbol  = this.player[1][1];
-  let currentPlayer = firstSymbol;
-  current.textContent = "Turn for " + currentPlayer;
-  (currentPlayer === firstSymbol) ?  current.style.color = "#BFCC94" : current.style.color = "#C1292E";
+  const firstPlayer = document.getElementById("firstPlayerName").value;
+  const secondPlayer = document.getElementById("secondPlayerName").value;
+  const firstPlayerSymbol = document.getElementById("firstPlayerSymbol").value;
+  const secondPlayerSymbol = document.getElementById("secondPlayerSymbol").value;
 
-  tiles.forEach(tile => {
-      tileArray.push(tile);
-
-      tile.addEventListener("click", () => {
-          if(tile.textContent !== ""){
-              return;
-          }
-
-          if(currentPlayer === "X"){
-              tile.style.color = "#BFCC94";
-          } if(currentPlayer === "O"){
-              tile.style.color = "#C1292E";
-          }
-          
-          tile.textContent = currentPlayer;
-
-          const win = winner(tileArray);
-
-          if(win === "X"){
-              console.log("X won");
-              return;
-          } else if(win === "O"){
-              console.log("O won");
-              return;
-          } else {
-              const draw = tileArray.every(tile => {
-                  return tile.textContent !== ""
-              });
-
-              if(draw){
-                  console.log("Draw");
-                  return;
-              }
-          }
-
-          currentPlayer = (currentPlayer === firstSymbol) ? secondSymbol : firstSymbol;
-          current.textContent = "Turn for " + currentPlayer;
-          (currentPlayer === firstSymbol) ?  current.style.color = "#BFCC94" : current.style.color = "#C1292E";
-      });
-  });
-}
-
-function stop(){
-  tiles.forEach(tile => {
-      tile.textContent = "";
-  });
-}
-
-function changeAnimation(first, second){
-  first.style.opacity = '0';
-  setTimeout(() => {
-    first.classList.add("hidden");
-    second.classList.remove("hidden");
-    second.style.opacity = '100';
-  }, 1000);
-}
-
-function getPlayer(action){
-  let firstPlayer = document.getElementById("firstPlayerName").value;
-  let secondPlayer = document.getElementById("secondPlayerName").value;
-  let firstPlayerSymbol = document.getElementById("firstPlayerSymbol").value;
-  let secondPlayerSymbol = document.getElementById("secondPlayerSymbol").value;
-
-  if(action === "stop"){
-    firstPlayer = "";
-    firstPlayerSymbol = "";
-    secondPlayer = "";
-    secondPlayerSymbol = "";
-    return;
-  }
-  
   if(firstPlayer === "" || secondPlayer === "" || firstPlayerSymbol === secondPlayerSymbol){
     entryError.classList.remove("hidden");
     setTimeout(() => {
       entryError.classList.add("hidden");
   }, 1000);
-    return false;
+    return;
   }
 
   const player1 = [firstPlayer, firstPlayerSymbol];
   const player2 = [secondPlayer, secondPlayerSymbol];
+  const playerList =[player1, player2];
 
-  const game = new Game(player1, player2);
+  firstSymbol = playerList[0][1];
+  secondSymbol  = playerList[1][1];
+  currentPlayer = firstSymbol;
 
-  return game;
+  current.textContent = "Turn for " + currentPlayer;
+  (currentPlayer === firstSymbol) ?  current.style.color = "#BFCC94" : current.style.color = "#C1292E";
+}
+
+function reset(){
+  const firstPlayer = document.getElementById("firstPlayerName");
+  const secondPlayer = document.getElementById("secondPlayerName");
+  const firstPlayerSymbol = document.getElementById("firstPlayerSymbol");
+  const secondPlayerSymbol = document.getElementById("secondPlayerSymbol");
+
+  tiles.forEach(tile => {
+      tile.textContent = "";
+      tile.style.color = "";
+  });
+
+  firstPlayer.value = "";
+  secondPlayer.value = "";
+  firstPlayerSymbol.selectedIndex = 0;
+  secondPlayerSymbol.selectedIndex = 0;
 }
 
 playBtn.addEventListener("click", () => {
-  const game = getPlayer();
-
-  if(!game){
-    return;
-  }
-
   changeAnimation(entryPlayer, gameBoard);
 
-
-  game.start();
+  play();
 });
 
 resetBtn.addEventListener("click", () => {
-  const game = getPlayer("stop");
-
   changeAnimation(gameBoard, entryPlayer);
 
-  game.stop();
+  reset();
+});
+
+let tileArray = [];
+let firstSymbol;
+let secondSymbol;
+let currentPlayer;
+
+tiles.forEach(tile => {
+    tileArray.push(tile);
+
+    tile.addEventListener("click", () => {
+        if(tile.textContent !== ""){
+            return;
+        }
+
+        if(currentPlayer === "X"){
+            tile.style.color = "#BFCC94";
+        } if(currentPlayer === "O"){
+            tile.style.color = "#C1292E";
+        }
+        
+        tile.textContent = currentPlayer;
+
+        const win = winner(tileArray);
+
+        if(win === "X"){
+            console.log("X won");
+            return;
+        } else if(win === "O"){
+            console.log("O won");
+            return;
+        } else {
+            const draw = tileArray.every(tile => {
+                return tile.textContent !== ""
+            });
+
+            if(draw){
+                console.log("Draw");
+                return;
+            }
+        }
+
+        currentPlayer = (currentPlayer === firstSymbol) ? secondSymbol : firstSymbol;
+        current.textContent = "Turn for " + currentPlayer;
+        (currentPlayer === firstSymbol) ?  current.style.color = "#BFCC94" : current.style.color = "#C1292E";
+    });
 });
